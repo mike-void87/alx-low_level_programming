85e46d934cdd60a99b18b4730614ea128f9fe1ea
mike <michaelvoid11@gmail.com>
2023-02-17 06:11:47 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,25 +8,21 @@
 int main(void)
 {
 	int i = 0;
+	int j;
 
 	while (i < 10)
 	{
-		int j = i + 1;
-
+		j = i + 1;
 		while (j < 10)
 		{
 			putchar(i + '0');
-			putchar(j + '0');
-
-			if (i < 8 || j < 9)
+			putchar(j++ + '0');
+			if (i < 8)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-
-			j++;
 		}
-
 		i++;
 	}
 
@@ -34,5 +30,4 @@ int main(void)
 
 	return (0);
 }
-
 
