1b1e6d69cf477f01a04dc8067625a724709c27f0
mike <michaelvoid11@gmail.com>
2023-02-21 10:53:40 -0800

    islewer

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -5,16 +6,16 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int  main(void)
 {
     int r;
 
     r = _islower('H');
-    _putchar(r + '0');
+    putchar(r + '0');
     r = _islower('o');
-    _putchar(r + '0');
+    putchar(r + '0');
     r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
+    putchar(r + '0');
+    putchar('\n');
     return (0);
 }
