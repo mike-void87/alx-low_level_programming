358173a25648d5cfd3b43c9a3f393336c3123e39
Mikevoid87 <michaelvoid11@gmailcom>
2023-04-06 06:08:40 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,30 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * the_function - prints the binary equivalent of a decimal number
+ * print_binary - prints the binary equivalent of a decimal number
  * @w: number to print in binary
  */
-void the_function(unsigned int w)
+void print_binary(unsigned long  int w)
 {
 	int v = 0;
 	int mob = 0;
 
-	unsigned int my_ market;
+	unsigned long int market;
 
-	for (v = 0; >= 0; v--)
+	for (v = 0; v >= 0; v--)
 	{
-		my_market = v >> i;
+		market = w  >> v;
 
-		if (my_market & 1)
+		if (market & 1)
 		{
-			_putchar("1");
+			_putchar('1');
 			mob++;
 		}
-		else if (my_market)
-			_putchar("0");
+		else if (market)
+			_putchar('0');
 	}
-	if (my_market == NULL)
-		_putchar("0");
+	if (!market)
+		_putchar('0');
 }
 
 
